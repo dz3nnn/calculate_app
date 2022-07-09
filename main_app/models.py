740e508d799ecec1b14797f8a964c14e885ea528
Dzianis <ulybqa@gmail.com>
2022-07-09 10:00:15 +0300

    add model

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Item(models.Model):
+    """ Модель для товаров """
+    name = models.CharField(verbose_name='Наименование', max_length=100)
+    count = models.IntegerField(verbose_name='Количество')
+    price = models.DecimalField(verbose_name='Цена закупки')
