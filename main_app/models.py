b4b72335473fb3d13f79b91f95529a00d83a404c
Dzianis <ulybqa@gmail.com>
2022-07-14 14:29:57 +0300

    inc max_digits for price

--- main_app/models.py
+++ main_app/models.py
@@ -20,7 +20,7 @@ class Item(models.Model):
     name = models.CharField(verbose_name='Наименование', max_length=100)
     count = models.IntegerField(verbose_name='Количество')
     price = models.DecimalField(
-        verbose_name='Цена закупки', max_digits=4, decimal_places=2)
+        verbose_name='Цена закупки', max_digits=10, decimal_places=2)
     invoice = models.ForeignKey(
         Invoice, on_delete=models.CASCADE, verbose_name='Накладная', blank=True, null=True)
     created_date = models.DateField(verbose_name='Дата', default=now)
