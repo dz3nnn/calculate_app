91e86717fb23986152e76652975d1f70600821ff
Dzianis <ulybqa@gmail.com>
2022-07-14 11:42:58 +0300

    upd str for item model

--- main_app/models.py
+++ main_app/models.py
@@ -8,7 +8,10 @@ class Item(models.Model):
     count = models.IntegerField(verbose_name='Количество')
     price = models.DecimalField(
         verbose_name='Цена закупки', max_digits=4, decimal_places=2)
-    created_date = models.DateField(verbose_name='Дата',default=now)
+    created_date = models.DateField(verbose_name='Дата', default=now)
+
+    def __str__(self):
+        return '%s (количество: %s) - %s' % (self.name, self.count, self.created_date)
 
     class Meta:
         verbose_name = 'Товар'
