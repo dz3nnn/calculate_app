67f17ffd187a6320e4ac9c961f7ba192fa0f1845
Dzianis <ulybqa@gmail.com>
2022-07-14 16:11:40 +0300

    test try

--- main_app/tests.py
+++ main_app/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
+from .models import Invoice
 
-# Create your tests here.
+
+class InvoiceTestCase(TestCase):
+    def setUp(self) -> None:
+        Invoice.objects.create(name='testing')
+
+    def test_full_price(self):
+        invoice = Invoice.objects.get(name='testing')
+        self.assertEqual(invoice.full_price, 0)
