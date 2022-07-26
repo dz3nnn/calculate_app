8fee09ecb25abf31b337639c1809bb5a2fced15b
Dzianis <ulybqa@gmail.com>
2022-07-26 12:28:08 +0300

    new models

--- main_app/models.py
+++ main_app/models.py
@@ -28,6 +28,7 @@ class Item(models.Model):
     """ Модель для товаров """
     name = models.CharField(verbose_name='Наименование', max_length=100)
     count = models.IntegerField(verbose_name='Количество')
+    rest = models.IntegerField(verbose_name='Остаток', default=0)
     price = models.DecimalField(
         verbose_name='Цена закупки', max_digits=10, decimal_places=2)
     invoice = models.ForeignKey(
@@ -40,3 +41,33 @@ class Item(models.Model):
     class Meta:
         verbose_name = 'Товар'
         verbose_name_plural = 'Товары'
+
+    def save(self, *args, **kwargs):
+        if not self.rest:
+            self.rest = self.count
+        super(Item, self).save(*args, **kwargs)
+
+
+class Sell(models.Model):
+    """ Модель продажи товара """
+
+    item = models.ForeignKey(
+        Item, on_delete=models.CASCADE, verbose_name='Товар', blank=True, null=True)
+    count = models.IntegerField(verbose_name='Количество')
+    price = models.DecimalField(
+        verbose_name='Цена продажи', max_digits=10, decimal_places=2)
+
+    class Meta:
+        verbose_name = 'Продажа'
+        verbose_name_plural = 'Продажи'
+
+    def save(self, *args, **kwargs):
+        if self.item:
+            item_model = Item.objects.get(pk=self.item.pk)
+            if item_model.rest >= self.count:
+                item_model.rest -= self.count
+                item_model.save()
+                super(Sell, self).save(*args, **kwargs)
+        else:
+            # Can't sell
+            pass
