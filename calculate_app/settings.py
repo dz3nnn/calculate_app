d82e8c8e5059532ddad103e41b697b86608715f9
Dzianis Zhoh <ulybqa@gmail.com>
2022-08-31 13:55:00 +0300

    setup debug env var

--- calculate_app/settings.py
+++ calculate_app/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get(
     'SECRET_KEY', 'insecure-key-517231hbdahbsdghasdjhasdjkasfnas')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG_VALUE') == 'TRUE'
 
 ALLOWED_HOSTS = ['*']
 
