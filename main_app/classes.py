5e78dad5211b94e537209c5c32eddbf52fdc023c
Dzianis Zhoh <ulybqa@gmail.com>
2022-09-22 12:59:36 +0300

    add month calendar

---
+++ main_app/classes.py
@@ -0,0 +1,7 @@
+from xmlrpc.client import Boolean
+
+
+class MonthItem:
+    def __init__(self, name, price):
+        self.name = name
+        self.price = price
--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,9 @@
 from django.http import JsonResponse
-from .models import Item
+from .models import Item, Sell, Payment, Service
 from django.shortcuts import render
+from .classes import MonthItem
+
+import datetime
 
 
 def setup_rests(request):
@@ -20,4 +23,67 @@ def index(request):
 
 
 def month(request):
-    return render(request, 'month.html', context={})
+    items = []
+    q = request.GET.get("q", None)
+
+    if q:
+        filter_date = _parse_date_from_q(q)
+        sells = Sell.objects.filter(created_date__year__gte=filter_date.year,
+                                    created_date__month__gte=filter_date.month,
+                                    created_date__year__lte=filter_date.year,
+                                    created_date__month__lte=filter_date.month)
+        for sell in sells:
+            item = _prepare_sell_to_month_item(sell)
+            items.append(item)
+
+        payments = Payment.objects.filter(created_date__year__gte=filter_date.year,
+                                          created_date__month__gte=filter_date.month,
+                                          created_date__year__lte=filter_date.year,
+                                          created_date__month__lte=filter_date.month)
+
+        for payment in payments:
+            item = _prepare_payment_to_month_item(payment)
+            items.append(item)
+
+        services = Service.objects.filter(created_date__year__gte=filter_date.year,
+                                          created_date__month__gte=filter_date.month,
+                                          created_date__year__lte=filter_date.year,
+                                          created_date__month__lte=filter_date.month)
+
+        for service in services:
+            item = _prepare_service_to_month_item(service)
+            items.append(item)
+
+    return render(request, 'month.html', context={'items': items, 'full_price': _calculate_full_price(items)})
+
+
+def _parse_date_from_q(q):
+    return datetime.datetime.strptime(q, "%Y-%m").date()
+
+
+def _prepare_sell_to_month_item(sell):
+    item = MonthItem(name=f'{sell.item.name} ({sell.count} шт.)',
+                     price=_calc_sell_value(sell))
+    return item
+
+
+def _prepare_service_to_month_item(service_model):
+    item = MonthItem(name=service_model.name, price=service_model.price)
+    return item
+
+
+def _prepare_payment_to_month_item(payment_model):
+    item = MonthItem(name=payment_model.payment_type.name,
+                     price=-abs(payment_model.price))
+    return item
+
+
+def _calc_sell_value(sell_model):
+    return sell_model.count * (sell_model.price - sell_model.item.price)
+
+
+def _calculate_full_price(items):
+    full_price = 0
+    for item in items:
+        full_price += item.price
+    return full_price
